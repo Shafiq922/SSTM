a72add72fe71a3624ba769bb840bd207d987e955
Shafiq <shafiqaiman922@gmail.com>
2026-02-11 01:39:32 +0800

    4th

--- database/seeders/SLATableSeeder.php
+++ database/seeders/SLATableSeeder.php
@@ -12,29 +12,36 @@ class SLATableSeeder extends Seeder
         DB::table('sla')->upsert([
             [
                 'priority' => 'Critical',
-                'response_time_minutes' => 60,     // 1 hour
+                'response_time_minutes' => 30,     // 1 hour
                 'resolution_time_minutes' => 240,  // 4 hours
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
             [
                 'priority' => 'High',
-                'response_time_minutes' => 240,    // 4 hours
-                'resolution_time_minutes' => 1440, // 1 day
+                'response_time_minutes' => 60,    // 4 hours
+                'resolution_time_minutes' => 480, // 1 day
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
             [
                 'priority' => 'Normal',
-                'response_time_minutes' => 480,    // 8 hours
-                'resolution_time_minutes' => 4320, // 3 days
+                'response_time_minutes' => 240,    // 8 hours
+                'resolution_time_minutes' => 2880, // 3 days
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
             [
                 'priority' => 'Low',
+                'response_time_minutes' => 480,   // 1 day
+                'resolution_time_minutes' => 4320,// 7 days
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'priority' => 'Planning',
                 'response_time_minutes' => 1440,   // 1 day
-                'resolution_time_minutes' => 10080,// 7 days
+                'resolution_time_minutes' => 7200,// 7 days
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
